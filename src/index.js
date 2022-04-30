61699c4c4637fde77385b73e9c64b0e87100f410
PercTruman <drshawnhart@gmail.com>
2022-04-30 17:08:08 -0500

    got all essentials working...time for bonus?

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ document.addEventListener('DOMContentLoaded',
     .then(res=>res.json())
     .then(ramenDataObjects=>renderRamenImages(ramenDataObjects))
 )
+document.getElementById('submitBtn').addEventListener('click', (e)=>{ 
+    e.preventDefault()
+    let nameValue =document.getElementById('new-name').value
+    let restaurantValue= document.getElementById('new-restaurant').value
+    let imageValue = document.getElementById('new-image').value
+    let ratingValue = document.getElementById('new-rating').value
+    let commentValue =document.getElementById('new-comment').value
+
+
+    let ramenObject= {
+      name: nameValue, 
+      restaurant: restaurantValue,
+      image: imageValue,
+      rating: ratingValue,
+      comment: commentValue
+    }
+    createNewRamenEntry(ramenObject)
+    
+  })
 
 function renderRamenImages(ramenObjects){
   const ramenMenuBar=document.getElementById('ramen-menu')
@@ -17,7 +36,6 @@ function renderRamenImages(ramenObjects){
 }
 
 function displayIndividualRamenInfo(ramenObject){
-    // let focusedInfoDiv =document.getElementById('ramen-detail')
     let focusedRamenPic =document.getElementById('detail-image')
         focusedRamenPic.src = ramenObject.image
     let focusedRamenName = document.getElementById('name')
@@ -29,6 +47,29 @@ function displayIndividualRamenInfo(ramenObject){
     let focusedRamenComment = document.getElementById("comment-display")
         focusedRamenComment.innerText = ramenObject.comment
 }
+
+function createNewRamenEntry(ramenObject){
+   return fetch('http://localhost:3000/ramens',{
+        method: 'POST',
+        headers:{
+            "Content-Type":"application/json",
+            Accept: "application/json"
+            },
+        body:JSON.stringify(ramenObject)
+          })
+    .then(res =>res.json())
+    .then(ramen=>addNewRamen(ramen))
+    }
+
+function addNewRamen(ramen){
+    const ramenMenuBar=document.getElementById('ramen-menu')
+    let ramenPicture =document.createElement('img')
+        ramenPicture.src = ramen.image
+    
+    ramenMenuBar.append(ramenPicture)
+        ramenPicture.addEventListener('click', ()=>displayIndividualRamenInfo(ramen))
+    }
+
 
 
  
